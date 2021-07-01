6efd19054e32ef6fbba6e7533411934abd0a0f5b
DESKTOP-EF5KG0E\steph <stephenmarcus123@gmail.com>
2021-06-30 23:36:45 -0700

    Docs & images

--- ModernAppDemo/ViewModels/CreateAccountViewModel.cs
+++ ModernAppDemo/ViewModels/CreateAccountViewModel.cs
@@ -26,182 +26,170 @@
 // SOFTWARE.
 // *********************************************************************************
 
-using Com.MarcusTS.SharedUtils.Utils;
+using System;
 using System.Threading.Tasks;
 using Com.MarcusTS.ResponsiveTasks.XamFormsSupport.Common.Utils;
+using Com.MarcusTS.ResponsiveTasks.XamFormsSupport.ViewModels;
+using Com.MarcusTS.SharedForms.Common.Behaviors;
+using Com.MarcusTS.SharedForms.Common.Interfaces;
+using Com.MarcusTS.SharedForms.ViewModels;
+using Com.MarcusTS.SharedUtils.Utils;
+using ModernAppDemo.Common.Interfaces;
+using ModernAppDemo.Common.Utils;
+using SQLite;
 
 namespace ModernAppDemo.ViewModels
 {
-    using Com.MarcusTS.ResponsiveTasks.XamFormsSupport.ViewModels;
-    using Com.MarcusTS.SharedForms.Common.Behaviors;
-    using Com.MarcusTS.SharedForms.Common.Interfaces;
-    using Com.MarcusTS.SharedForms.ViewModels;
-    using Common.Interfaces;
-    using Common.Utils;
-    using SQLite;
-    using System;
+   public interface ICreateAccountViewModel : IWizardViewModelWithTasks, ICommonAccountProps,
+      IHandleNullableDateTimeChanges
+   {
+   }
 
-    public interface ICreateAccountViewModel : IWizardViewModelWithTasks, ICommonAccountProps,
-       IHandleNullableDateTimeChanges
-    {
-    }
+   public class CreateAccountViewModel : WizardViewModelWithTasks, ICreateAccountViewModel
+   {
+      private string _confirmPassword;
+      private string _firstName;
+      private bool _isTheSkyBlue;
+      private string _lastName;
+      private string _password;
+      private DateTime? _serviceStartDate;
+      private string _stateOfResidence;
+      private string _userName;
 
-    public class CreateAccountViewModel : WizardViewModelWithTasks, ICreateAccountViewModel
-    {
-        private string _confirmPassword;
-        private string _firstName;
-        private bool _isTheSkyBlue;
-        private string _lastName;
-        private string _password;
-        private DateTime? _serviceStartDate;
-        private string _stateOfResidence;
-        private string _userName;
+      public CreateAccountViewModel()
+      {
+         Title = "Create New Account";
 
-        public CreateAccountViewModel()
-        {
-            Title = "Create New Account";
+         // IMPORTANT
+         // Required to make the visual date time selector work.
+         // Without this setting, the selector shows DateTime.Now, but stores null.
+         ServiceStartDate = DateTime.Now.Date;
+      }
 
-            // IMPORTANT
-            // Required to make the visual date time selector work.
-            // Without this setting, the selector shows DateTime.Now, but stores null.
-            ServiceStartDate = DateTime.Now.Date;
-        }
-
-      [CommonViewModelValidations.ValidatableTwoWayNonEmptyViewModelValidationAttribute(displayOrder: 4,
-                                    IsPassword = ViewModelValidationAttribute_Static.TRUE_BOOL,
-                                    CanUnmaskPassword = ViewModelValidationAttribute_Static.TRUE_BOOL,
-                                    PlaceholderText = CommonViewModelValidations.CONFIRM_PASSWORD_PLACEHOLDER_TEXT,
-                                    ValidatorType = typeof(ComparisonEntryValidatorBehavior))]
+      [CommonViewModelValidations.ValidatableTwoWayNonEmptyViewModelValidationAttribute(4,
+         IsPassword = ViewModelValidationAttribute_Static.TRUE_BOOL,
+         CanUnmaskPassword = ViewModelValidationAttribute_Static.TRUE_BOOL,
+         PlaceholderText = CommonViewModelValidations.CONFIRM_PASSWORD_PLACEHOLDER_TEXT,
+         ValidatorType = typeof(ComparisonEntryValidatorBehavior))]
       public string ConfirmPassword
-        {
-            get => _confirmPassword;
-            set
-            {
-                if (SetProperty(ref _confirmPassword, value))
-                {
-                    // WARNING Invalid TPL root.
-                    VerifyCommandCanExecute().FireAndForget();
-                }
-            }
-        }
+      {
+         get => _confirmPassword;
+         set
+         {
+            if (SetProperty(ref _confirmPassword, value))
+               // WARNING Invalid TPL root.
+               VerifyCommandCanExecute().FireAndForget();
+         }
+      }
 
-        // For storage only
-        public string FirstAndLastName { get; set; }
-
-      [CommonViewModelValidations.ValidatableTwoWayNonEmptyViewModelValidationAttribute(displayOrder: 0, PlaceholderText = "First Name", ExcludedChars = CommonViewModelValidations.FORBIDDEN_CHARS)]
-      public string FirstName
-        {
-            get => _firstName;
-            set
-            {
-                if (SetProperty(ref _firstName, value))
-                {
-                    // WARNING Invalid TPL root.
-                    RefreshFirstAndLastName().FireAndForget();
-                }
-            }
-        }
-
-        // For storage only
-        [PrimaryKey] [AutoIncrement] public int Id { get; set; }
-
-      [CommonViewModelValidations.ValidatableTwoWayNonEmptyViewModelValidationAttribute(displayOrder: 7, PlaceholderText = "Is the Sky Blue?",
-                         InputTypeStr = InputTypes_RTXFS.InputTypes_RTFXS_CheckBoxInput)]
+      [CommonViewModelValidations.ValidatableTwoWayNonEmptyViewModelValidationAttribute(7,
+         PlaceholderText = "Is the Sky Blue?",
+         InputTypeStr = InputTypes_RTXFS.InputTypes_RTFXS_CheckBoxInput)]
       public bool IsTheSkyBlue
-        {
-            get => _isTheSkyBlue;
-            set
-            {
-                if (SetProperty(ref _isTheSkyBlue, value))
-                {
-                    // WARNING Invalid TPL root.
-                    VerifyCommandCanExecute().FireAndForget();
-                }
-            }
-        }
+      {
+         get => _isTheSkyBlue;
+         set
+         {
+            if (SetProperty(ref _isTheSkyBlue, value))
+               // WARNING Invalid TPL root.
+               VerifyCommandCanExecute().FireAndForget();
+         }
+      }
 
-      [CommonViewModelValidations.ValidatableTwoWayNonEmptyViewModelValidationAttribute(displayOrder: 0, PlaceholderText = "Last Name", ExcludedChars = CommonViewModelValidations.FORBIDDEN_CHARS)]
+      [CommonViewModelValidations.ValidatableTwoWayNonEmptyViewModelValidationAttribute(8,
+         PlaceholderText = "State of Residence?", InputTypeStr = InputTypes_RTXFS.InputTypes_RTFXS_StateInput)]
+      public string StateOfResidence
+      {
+         get => _stateOfResidence;
+         set
+         {
+            if (SetProperty(ref _stateOfResidence, value))
+               // WARNING Invalid TPL root.
+               VerifyCommandCanExecute().FireAndForget();
+         }
+      }
+
+      // For storage only
+      public string FirstAndLastName { get; set; }
+
+      [CommonViewModelValidations.ValidatableTwoWayNonEmptyViewModelValidationAttribute(0,
+         PlaceholderText = "First Name", ExcludedChars = CommonViewModelValidations.FORBIDDEN_CHARS)]
+      public string FirstName
+      {
+         get => _firstName;
+         set
+         {
+            if (SetProperty(ref _firstName, value))
+               // WARNING Invalid TPL root.
+               RefreshFirstAndLastName().FireAndForget();
+         }
+      }
+
+      // For storage only
+      [PrimaryKey] [AutoIncrement] public int Id { get; set; }
+
+      [CommonViewModelValidations.ValidatableTwoWayNonEmptyViewModelValidationAttribute(0,
+         PlaceholderText = "Last Name", ExcludedChars = CommonViewModelValidations.FORBIDDEN_CHARS)]
       public string LastName
-        {
-            get => _lastName;
-            set
-            {
-                if (SetProperty(ref _lastName, value))
-                {
-                    // WARNING Invalid TPL root.
-                    RefreshFirstAndLastName().FireAndForget();
-                }
-            }
-        }
+      {
+         get => _lastName;
+         set
+         {
+            if (SetProperty(ref _lastName, value))
+               // WARNING Invalid TPL root.
+               RefreshFirstAndLastName().FireAndForget();
+         }
+      }
 
-      [CommonViewModelValidations.PasswordValidatableTwoWayNonEmptyViewModelValidation(3)]
+      [CommonViewModelValidations.PasswordValidatableTwoWayNonEmptyViewModelValidationAttribute(3)]
       public string Password
-        {
-            get => _password;
-            set
-            {
-                if (SetProperty(ref _password, value))
-                {
-                    // WARNING Invalid TPL root.
-                    VerifyCommandCanExecute().FireAndForget();
-                }
-            }
-        }
+      {
+         get => _password;
+         set
+         {
+            if (SetProperty(ref _password, value))
+               // WARNING Invalid TPL root.
+               VerifyCommandCanExecute().FireAndForget();
+         }
+      }
 
-      [CommonViewModelValidations.PhoneValidatableTwoWayNonEmptyViewModelValidationAttribute(displayOrder: 5)]
+      [CommonViewModelValidations.PhoneValidatableTwoWayNonEmptyViewModelValidationAttribute(5)]
       public string Phone { get; set; }
 
-        [CommonViewModelValidations.DateTimeValidatableTwoWayNonEmptyViewModelValidationAttribute(displayOrder: 6, PlaceholderText = "Start Date")]
-        public DateTime? ServiceStartDate
-        {
-            get => _serviceStartDate;
-            set
-            {
-                if (SetProperty(ref _serviceStartDate, value))
-                {
-                    // WARNING Invalid TPL root.
-                    VerifyCommandCanExecute().FireAndForget();
-                }
-            }
-        }
+      [CommonViewModelValidations.DateTimeValidatableTwoWayNonEmptyViewModelValidationAttribute(6,
+         PlaceholderText = "Start Date")]
+      public DateTime? ServiceStartDate
+      {
+         get => _serviceStartDate;
+         set
+         {
+            if (SetProperty(ref _serviceStartDate, value))
+               // WARNING Invalid TPL root.
+               VerifyCommandCanExecute().FireAndForget();
+         }
+      }
 
-      [CommonViewModelValidations.ValidatableTwoWayNonEmptyViewModelValidationAttribute(displayOrder: 8, PlaceholderText = "State of Residence?", InputTypeStr = InputTypes_RTXFS.InputTypes_RTFXS_StateInput)]
-      public string StateOfResidence
-        {
-            get => _stateOfResidence;
-            set
-            {
-                if (SetProperty(ref _stateOfResidence, value))
-                {
-                    // WARNING Invalid TPL root.
-                    VerifyCommandCanExecute().FireAndForget();
-                }
-            }
-        }
-
-      [CommonViewModelValidations.UserNameValidatableTwoWayNonEmptyValidation(2)]
+      [CommonViewModelValidations.UserNameValidatableTwoWayNonEmptyValidationAttribute(2)]
       public string UserName
-        {
-            get => _userName;
-            set
-            {
-                if (SetProperty(ref _userName, value))
-                {
-                    // WARNING Invalid TPL root.
-                    VerifyCommandCanExecute().FireAndForget();
-                }
-            }
-        }
+      {
+         get => _userName;
+         set
+         {
+            if (SetProperty(ref _userName, value))
+               // WARNING Invalid TPL root.
+               VerifyCommandCanExecute().FireAndForget();
+         }
+      }
 
-        public void HandleNullableResultChanged(DateTime? dateTime)
-        {
-            ServiceStartDate = dateTime;
-        }
+      public void HandleNullableResultChanged(DateTime? dateTime)
+      {
+         ServiceStartDate = dateTime;
+      }
 
-        private async Task RefreshFirstAndLastName()
-        {
-            FirstAndLastName = FirstName?.Trim() + " " + LastName?.Trim();
-            await VerifyCommandCanExecute().WithoutChangingContext();
-        }
-    }
+      private async Task RefreshFirstAndLastName()
+      {
+         FirstAndLastName = FirstName?.Trim() + " " + LastName?.Trim();
+         await VerifyCommandCanExecute().WithoutChangingContext();
+      }
+   }
 }
--- ModernAppDemo_4.md
+++ ModernAppDemo_4.md
@@ -1,2 +1,218 @@
-# A Fully Automated ("Viewless") UI Using View Model Attributes
+# A Fully Automated ("Viewless") UI<BR/> Using View Model Attributes
+
+<img src="docs/create_account_screen.jpg" width="300" align="right" />
+
+This is the [Modern App Demo](https://github.com/marcusts/Com.MarcusTS.ModernAppDemo)'s <B>Create Account</B> View.  There is something quite unusual about it.  It does not contain a "view" in the classical sense. This is how that might look in older-style Xamarin:
+
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+<BR/>
+
+This screen leverages a <B>FlexViewWithTasks</B> from the [ResponsiveTasks.XamFormsSupport library](https://github.com/marcusts/Com.MarcusTS.ResponsiveTasks.XamFormsSupport). Here's the source code, digested a bit for clarity:
+
+``` csharp
+public class CreateAccountView : FlexViewWithTasks, ICreateAccountView
+{
+    protected override Task AfterSourceViewsLoaded()
+    {
+        // Set the comparison validator form the password
+        
+        ... code removed
+
+        ///////////////////////////////////////////////////////////////////////////////
+        Add the save and cancel buttons
+        ///////////////////////////////////////////////////////////////////////////////
+
+        var nextTabIndex = MasterAnimatedStackLayoutAsView.SourceViews.Count;
+         
+        var saveButton =
+            CreateButton(
+                         "Create Account", 
+                         (BindingContext as IWizardViewModelWithTasks)?.NextCommand, 
+                         nextTabIndex++, 
+                         true,
+                         useExtraTopSpace:true);
+
+        MasterAnimatedStackLayoutAsView.SourceViews.Add(saveButton as View);
+
+        var cancelButton =
+            CreateButton(
+                         "CANCEL".Expanded(), 
+                         (BindingContext as IWizardViewModelWithTasks)?.CancelCommand, 
+                         // ReSharper disable once RedundantAssignment
+                         nextTabIndex++,
+                         false,
+                         Color.Red, 
+                         null, 
+                         backColor:Color.Transparent);
+
+        MasterAnimatedStackLayoutAsView.SourceViews.Add(cancelButton as View);
+
+        return base.AfterSourceViewsLoaded();
+    }
+}
+```
+
+The <B>FlexViewWithTasks</B> is a generic view that displays any sort of "tile-like" sub-view that represents a single view model property.  It is only derived for customization. The Create Account View adds a password comparison validator and a few buttons for the user to tap.  That's the entire thing.
+
+The view model is also quite spare:
+
+``` csharp
+public class CreateAccountViewModel : WizardViewModelWithTasks, ICreateAccountViewModel
+{
+    ... code variables omitted
+    
+    public CreateAccountViewModel()
+    {
+        Title = "Create New Account";
+    }
+    
+    [CommonViewModelValidations.
+    ValidatableTwoWayNonEmptyViewModelValidationAttribute(
+    displayOrder: 4, 
+    IsPassword = ViewModelValidationAttribute_Static.TRUE_BOOL,
+    CanUnmaskPassword = ViewModelValidationAttribute_Static.TRUE_BOOL,
+    PlaceholderText = CommonViewModelValidations.CONFIRM_PASSWORD_PLACEHOLDER_TEXT,
+    ValidatorType = typeof(ComparisonEntryValidatorBehavior))]
+    public string ConfirmPassword
+    {
+        get => _confirmPassword;
+        set
+        {
+            if (SetProperty(ref _confirmPassword, value))
+            {
+                VerifyCommandCanExecute().FireAndForget();
+            }
+        }
+    }
+    
+    ... other properties below
+```
+
+Notice that the view model  only sets the property and <I>(if set)</I>, re-checks a command to activate the Submit button once the view has been legally completed by the user.  It doesn't do anything else.  How is that possible?
+
+The answer is <I>attributed programming</I>.  In this case, the custom attribute <B>ValidatableTwoWayNonEmptyViewModelValidationAttribute</B>. That's a long-winded derived atttribute based on the <B>ViewModelValidationAttribute</B> base class:
+
+``` csharp
+public class ViewModelValidationAttribute : Attribute, IViewModelValidationAttribute
+```
+
+The <B>ViewModelValidationAttribute</B> class declares a property for each of the possible attribute settings.  These include things like the binding mode for the view model, the size and shape of the input field, etc.  The class is not meant to do anything -- only to convert the user settings from the attributed view model into an actual class variable that can be used at run-time.
+
+## The Magic Trick Revealed
+
+Let's return to the automated view producer, the <B>FlexViewWithTasks</B>, to see how it digests an attributed view model.
+
+``` csharp
+public class FlexViewWithTasks : 
+    ShapeViewWithTasks, 
+    IFlexViewWithTasks, 
+    IShapeViewBase, 
+    ICanSetContentSafely, 
+    IProvidePostContentTasks, 
+    ICanSetBindingContextSafely, 
+    IProvidePostBindingTasks
+{
+    ... code omitted
+
+    ///////////////////////////////////////////////////////////////////////////////
+    This is the ResponsiveTasks equivalent of "Binding Context Changed" -- 
+    we call the RecreatUI method.
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    protected virtual async Task HandlePostBindingTask(IResponsiveTaskParams paramdict)
+        => await ThreadHelper.WithoutChangingContext(this.RecreateUI());
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    Builds the UI sub-views and adds them to the view's stack layout
+    ///////////////////////////////////////////////////////////////////////////////
+    private async Task RecreateUI()
+    {
+        ... code omitted
+        
+        ///////////////////////////////////////////////////////////////////////////
+        Use reflection to grab the custom attributes from the view model
+        ///////////////////////////////////////////////////////////////////////////
+        var propInfoDict =
+          BindingContext.GetType().
+          CreateViewModelValidationPropertiesDict<ViewModelValidationAttribute_RTXFS>();
+
+        var retViews = new List<View>();
+
+        if (propInfoDict.IsNotAnEmptyList())
+        {
+            _nextTabIndex = 0;
+            
+            ////////////////////////////////////////////////////////////////////
+            Create an editable entry (which may or may not be user-editable)
+               based on the attributes provided; add the stack layout.
+            ////////////////////////////////////////////////////////////////////
+            foreach (var keyValuePair in propInfoDict.OrderBy(
+               kvp => kvp.Value.DisplayOrder))
+            {
+                var result =
+                    CreateEditableEntry(
+                    keyValuePair.Key, 
+                    keyValuePair.Value, 
+                    _allBehaviors?.Count);
+                  
+                ... code omitted
+            }
+        }      
+    }
+}
+```
+
+The method call to <B>CreateEditableEntry</B> is also worth noting, as it in turn calls this method:
+
+``` csharp
+public virtual 
+    (IValidatableViewWithTasks, ICanBeValidWithTasks, int) 
+    CreateValidatableEditorsForAttributeWithTasks
+    (
+        IHaveValidationViewModelHelperWithTasks viewModel,
+        IViewModelValidationAttribute_RTXFS attribute,
+        double itemHeight,
+        double itemWidth,
+        double fontSize,
+        ref int nextTabIndex
+    )
+    {
+        ... code omitted
+    
+        switch (attribute.InputTypeStr)
+        {
+            case InputTypes_RTXFS.InputTypes_RTFXS_StateInput:
+               return SetUpValidatablePicker(FormsUtils.STATIC_STATES, nextTabIndex);
+```
+
+The method determines what sort of editor to create based on the input type which is passed in by the view model attribute.
+
+## How (and When) To Use THIS Sort of "Trick"
+
+Philosophically, Xamarin should be made up of MVVM-style <B><I>separated concerns</I></B>.  This approach bends the rules slightly. The view model drives many details about <B><I>how</I></B> the view appears. On the other hand, sometimes the UI view is just a set of rote properties that are mostly similar to each other.  This actually helps the user to understand what to do.  The view model can provide these basic attributes, and the view can be built without the heavy-duty approach that would otherwise be required.  So use this trick for entry/input views such as Login or Create Account.
+
+
+
+
+
+
+
+
+
+
 
